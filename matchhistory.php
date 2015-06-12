d6353ba430918559204d9a05c9afd767ebae33e8
William <w.cameron@crookedhill.co.nz>
2015-06-12 23:07:57 +1200

    made the matchhistory return array instead of object

--- matchhistory.php
+++ matchhistory.php
@@ -7,6 +7,6 @@ function getMatchHistoryBySummonerId($region, $summonerId, $apiKey)
 {
   $service_url = "https://$region.api.pvp.net/api/lol/$region/v2.2/matchhistory/$summonerId?api_key=$apiKey";
   $response = file_get_contents($service_url);
-  return json_decode($response);
+  return json_decode($response, true);
 }
 ?>
